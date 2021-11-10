ff4fce271c9474da485efae681b4bfeca23fb969
Chris Lawson <github@chris2fourlaw.me>
2021-11-10 11:56:08 -0800

    Implement alerts and add (disabled) Slack Incident Checker

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,14 +1,40 @@
-import { app, BrowserWindow } from 'electron';
+import {
+    app, BrowserWindow, ipcMain,
+} from 'electron';
 import * as path from 'path';
+import { AlertLevel } from './util/alert';
 
-function createWindow() {
+import AlertHandler from './util/alerthandler';
+import SlackHandler from './util/slack';
+
+const { SLACK_TOKEN } = process.env;
+
+let mainWindow: BrowserWindow;
+const alertHandler = new AlertHandler();
+// const slackHandler = new SlackHandler(alertHandler, SLACK_TOKEN); // commented out until we get a token
+
+async function createWindow() {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         fullscreen: true,
         webPreferences: {
+            nodeIntegration: false, // is default value after Electron v5
+            contextIsolation: true, // protect against prototype pollution
             preload: path.join(__dirname, 'preload.js'),
         },
     });
+
+    enum DashState {
+        opening,
+        closing,
+        loop,
+        alert,
+        none
+    }
+
+    let currentState = DashState.none;
+    let prevState = DashState.none;
+    let urlCount = 0;
 
     const urls = [
         'http://solidus.calpoly.edu/WebApps/ContactCenter/WallDisplayScreens/Display?current=s458263',
@@ -17,26 +43,81 @@ function createWindow() {
     ];
 
     // To setup JIRA or Aruba, uncomment the respective choice and comment out the setInterval block
-    // mainWindow.loadURL(https://dashboard.capenetworks.com); // Aruba
-    // mainWindow.loadURL(https://calpoly.atlassian.net/secure/Dashboard.jspa?selectPageId=10190); // JIRA
+    // mainWindow.loadURL('https://dashboard.capenetworks.com'); // Aruba
+    // mainWindow.loadURL('https://calpoly.atlassian.net/secure/Dashboard.jspa?selectPageId=10190'); // JIRA
 
-    let urlCount = 0;
     setInterval(() => {
+        // Set state accordingly for opening/closing/alert/loop
         const minute = (new Date()).getMinutes();
         const hour = (new Date()).getHours();
+        const currentAlert = alertHandler.getCurrentAlert();
 
-        // Show open or close at the beginning or end of the day, otherwise cycle
-        if (hour === 8 && minute < 15) {
-            mainWindow.loadFile(path.join(__dirname, '../src/html/open.html'));
+        if (currentAlert) {
+            currentState = DashState.alert;
+        } else if (hour === 8 && minute < 15) {
+            currentState = DashState.opening;
         } else if (hour === 16 && minute > 50) {
-            mainWindow.loadFile(path.join(__dirname, '../src/html/close.html'));
+            currentState = DashState.closing;
         } else {
+            currentState = DashState.loop;
+        }
+
+        // Choose what to diplay
+        switch (currentState) {
+        case DashState.opening:
+            if (prevState !== DashState.opening) mainWindow.loadFile(path.join(__dirname, '../src/html/open.html'));
+            prevState = DashState.opening;
+            break;
+        case DashState.closing:
+            if (prevState !== DashState.closing) mainWindow.loadFile(path.join(__dirname, '../src/html/close.html'));
+            prevState = DashState.closing;
+            break;
+        case DashState.loop:
             mainWindow.loadURL(urls[urlCount]);
             urlCount += 1;
             if (urlCount > urls.length - 1) urlCount = 0;
+            prevState = DashState.loop;
+            break;
+        case DashState.alert:
+            mainWindow.loadFile(path.join(__dirname, '../src/html/alert.html'));
+            prevState = DashState.alert;
+            break;
+        default:
+            break;
         }
-    }, 10000);
+    }, 10000); // Repeat every 10 seconds
 }
+
+// Send alerts info if needed
+ipcMain.on('alert-page-ready', (event) => {
+    if (alertHandler.getCurrentAlert()) {
+        event.sender.send('alert-title', alertHandler.getCurrentAlert().getTitle().toString());
+        event.sender.send('alert-desc', alertHandler.getCurrentAlert().getDescription().toString());
+        event.sender.send('alert-level', alertHandler.getCurrentAlert().getLevel());
+    }
+});
+
+// Slack incident checker
+const incidentChannelID = 'CM8KV8A1M';
+let prevMessageText: string;
+
+// commented out until we get a token
+
+// slackHandler.getLatestMessage(incidentChannelID).then((response) => {
+//     prevMessageText = response.messages[0].text;
+// });
+// setInterval(() => {
+//     slackHandler.getLatestMessage(incidentChannelID).then((response) => {
+//         const responseText: string = response.messages[0].text;
+//         if (responseText !== prevMessageText) {
+//             alertHandler.raiseAlert(AlertLevel.warning, 'New Incident Raised!', responseText);
+//         }
+//     });
+// }, 10000);
+
+//
+// Electron stuff
+//
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -59,6 +140,3 @@ app.on('window-all-closed', () => {
         app.quit();
     }
 });
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/js/preload.ts
+++ src/js/preload.ts
@@ -1,4 +1,43 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+import { ipcRenderer } from 'electron';
+import { AlertLevel } from './util/alert';
+
+const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector);
+    if (element) {
+        element.innerText = text;
+    }
+};
+
+ipcRenderer.on('alert-title', (event, response) => {
+    replaceText('alert-title', response);
+});
+
+ipcRenderer.on('alert-desc', (event, response) => {
+    replaceText('alert-description', response);
+});
+
+ipcRenderer.on('alert-level', (event, response: AlertLevel) => {
+    const body = document.getElementById('body');
+    switch (response) {
+    case AlertLevel.info:
+        body.classList.add('bg-info');
+        break;
+    case AlertLevel.warning:
+        body.classList.add('bg-warning');
+        break;
+    case AlertLevel.critical:
+        body.classList.add('bg-danger');
+        break;
+    default:
+        break;
+    }
+});
+
 window.addEventListener('DOMContentLoaded', () => {
+    if (window.location.href.includes('alert.html')) {
+        ipcRenderer.send('alert-page-ready', true);
+    }
 });
